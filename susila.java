1a8bedfa1096542f4078a7a41ec042f7fb1375ff
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-01-10 16:27:36 +0000

    susila.java

---
+++ susila.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class susila  
+{
+public stati void main(String[]args)
+{
+Scanner sc=new scanner ();
+System.out.println("enter the values");
+int n= sc.nextInt();
+if(n/2==0)
+{
+System.out.println("it is even");
+{
+els{
+System.out.println("it is odd");
+System.out.println(n);
+}
+}
+}
+}
